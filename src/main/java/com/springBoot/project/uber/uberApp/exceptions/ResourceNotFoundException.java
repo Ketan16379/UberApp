11344d377cc5eca9aef8549ff723784a75f29d1e
Ketan16379 <ketanmeshram184@gmail.com>
2025-11-22 14:34:04 +0530

    Adding the advice package with all the necessary classes

---
+++ src/main/java/com/springBoot/project/uber/uberApp/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+package com.springBoot.project.uber.uberApp.exceptions;
+
+public class ResourceNotFoundException {
+}
---
+++ src/main/java/com/springBoot/project/uber/uberApp/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.springBoot.project.uber.uberApp.exceptions;
+
+public class RuntimeConflitException extends RuntimeException {
+
+    public RuntimeConflitException() {
+    }
+
+    public RuntimeConflitException(String message) {
+        super(message);
+    }
+}
